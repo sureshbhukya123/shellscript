5cd390e93d2636b692b2f7a8c58f800266dc7355
sureshbhukya123 <103753987+sureshbhukya123@users.noreply.github.com>
2022-05-06 10:09:23 +0530

    add two numbers

--- shellscript4.sh
+++ shellscript4.sh
@@ -7,7 +7,7 @@ echo -e "enter the first_number:\c"
 read first_number
 echo -e "enter second_number:\c"
 read second_number
-sum= $(($first_number + $second_number))
+sum=$(( $first_number + $second_number ))
 echo "sum of $first_number and $second_number is: $sum"
 
 
